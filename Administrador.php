d1862910edfafd4cfea0b7ce8f07f4abbb79d457
aamirXD <52842252+aamirXD@users.noreply.github.com>
2025-03-14 21:48:26 -0500

    Administrador.php

---
+++ Administrador.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0.1">
+	<title>Página de administración</title>
+	<link rel="stylesheet" type="text/css" href="StyleSheetTW.CSS">
+</head>
+<body>
+	<header>
+	<h1>Reservas realizadas</h1>
+	</header>
+
+<?php
+$cn = mysqli_connect("localhost", "root", "", "reservas_bd");
+
+$sql = "SELECT * FROM clientes";
+$fila = mysqli_query($cn, $sql);
+
+echo "<table border='0' align='center'>";
+echo "<tr>
+		<th>Nombre</th>
+		<th>Teléfono</th>
+		<th>Fecha</th>
+		<th>Hora</th>
+		<th>Cantidad</th>
+		<th>Selección</th>
+		<th>Modificar reserva</th>
+	</tr>";
+
+while ($r = mysqli_fetch_array($fila)) {
+    echo "<tr align='center'>
+			<td>{$r['user_name']}</td>
+			<td>{$r['user_tel']}</td>
+			<td>{$r['user_date']}</td>
+			<td>{$r['user_time']}</td>
+			<td>{$r['user_cant']}</td>
+			<td>
+				<form method='POST' action='Eliminar.php'>
+					<input type='hidden' name='id' value='{$r['user_cod']}'>
+					<input type='submit' value='Eliminar'>
+				</form>
+			</td>
+			<td>
+				<form align='center' method='POST' action='Modificar.php'>
+					<input type='hidden' name='id' value='{$r['user_cod']}'>
+					<input type='text' name='txtnombre' value='{$r['user_name']}'>
+					<input type='tel' name='txttelefono' value='{$r['user_tel']}'>
+					<input type='date' name='txtfecha' value='{$r['user_date']}'>
+					<input type='time' name='txthora' value='{$r['user_time']}'>
+					<input type='number' min='1' max='4' name='txtcantidad' value='{$r['user_cant']}'>
+					<input type='submit' name='enviar' value='guardar datos'>
+				</form>
+			</td>
+		  </tr>";
+}
+echo "</table>";
+mysqli_close($cn);
+?>
+</body>
+</html>
